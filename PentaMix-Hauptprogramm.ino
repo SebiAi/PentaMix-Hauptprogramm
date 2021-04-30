3af7295d5cea63421b35ae475d963b295e9cb954
SebiAi <sebastian.ai@gmx.at>
2021-04-30 17:47:29 +0200

    removed TODO

--- PentaMix-Hauptprogramm.ino
+++ PentaMix-Hauptprogramm.ino
@@ -26,7 +26,7 @@ const uint8_t numPumps = sizeof(pumpPins) / sizeof(pumpPins[0]);
 
 // Arrays
 Button buttons[numButtons];
-Pump pumps[numPumps]; // TODO: Pump class
+Pump pumps[numPumps];
 
 // Display
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
