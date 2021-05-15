b4f0fb69a6155331e55f0a299e6bc802930a9a2c
SebiAi <sebastian.ai@gmx.at>
2021-05-16 01:00:45 +0200

    Config description updated

--- PentaMix-Hauptprogramm.ino
+++ PentaMix-Hauptprogramm.ino
@@ -19,12 +19,12 @@ Changelog:
 #include "defines.h"
 
 // Config
-#define NUMDRINKS 5
-#define CUPSIZEML 330                                       // Bechergröße in ml
-#define NUMPARTS 10                                         // Anzahl an verschiedenen Anteilen
-const uint8_t buttonPins[] = {2, 3, 4, 5, 6, 7, 8};         // 0 bis 4 - Getränk 1 bis 5, 5 - Undo, 6 Ok
-const uint8_t pumpPins[NUMDRINKS] = {13, 12, 11, 10, 9};    // 0 bis 4 - Getränk 1 bis 5
-const uint8_t partsLinesXLocation[NUMPARTS] = {13, 26, 39, 52, 65, 78, 91, 103, 115, 127};
+#define NUMDRINKS 5                                                                         // Anzahl an Drinks
+#define CUPSIZEML 330                                                                       // Bechergröße in ml
+#define NUMPARTS 10                                                                         // Anzahl an verschiedenen Anteilen
+const uint8_t buttonPins[] = {2, 3, 4, 5, 6, 7, 8};                                         // 0 bis 4 - Getränk 1 bis 5, 5 - Undo, 6 Ok
+const uint8_t pumpPins[NUMDRINKS] = {13, 12, 11, 10, 9};                                    // 0 bis 4 - Getränk 1 bis 5
+const uint8_t partsLinesXLocation[NUMPARTS] = {13, 26, 39, 52, 65, 78, 91, 103, 115, 127};  // Definiert die X-Position für die Trennstriche der Teile
 // End Config
 
 // Längen holen
